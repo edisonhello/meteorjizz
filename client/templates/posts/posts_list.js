24ef409d6354781342664c9d946c5bcbcda40fc4
edisonhello <edisonhello@hotmail.com>
2015-12-21 12:47:47 +0800

    is not defined

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,18 @@
+var postsData=[
+	{
+		title: 'Introsldfjjsldkfjsldf',
+		url:'http://sachsdlsdfioehowihegwoeihgweoighweg'
+	},
+	{
+		title: 'meteor',
+		url:'http://meteor'
+	},
+	{
+		title:'The Meeergwrgwegw',
+		url:'http://themebosjdnfoeifqe'
+	}
+];
+
+Template.postsList.helpers({
+	posts: postsData
+});
